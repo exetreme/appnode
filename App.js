a252caf5d4e717bd63e17a1a45e60950288aee2a
github_exetreme <tokyoghoulge@gmail.com>
2022-05-22 01:06:26 +0700

    webview

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './components/Welcome';
+import {WebView} from 'react-native-webview';
 
 export default function App() {
   const names = ['ryan','ren', 'david'];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="dark" />
-      {names.map((name)=>(
-        <Welcome name={name}/>
-      ))}
+
+    <WebView style={{}} source={{uri:'https://codecontinue.com'}}/>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="dark" />
+    //   {names.map((nam)=>(
+    //     <Welcome nam={nam}/>
+    //   ))}
       
-    </View>
+    // </View>
   );
 }
 
